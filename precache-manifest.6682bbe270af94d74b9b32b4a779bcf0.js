5ab5d2572576e9fd0478b85d7e4e09c704f817c1
Maamoun Toujani <maamountoujani@Maamouns-Air.home>
2020-12-03 21:36:35 +0100

    Updates

---
+++ precache-manifest.6682bbe270af94d74b9b32b4a779bcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddec368a4dcddced77b684a26de22246",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7a434d22134bfe8504c",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "0b1b5dbfdbb20043e9b7",
+    "url": "./static/js/2.d50baf79.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d50baf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a434d22134bfe8504c",
+    "url": "./static/js/main.2ae912f2.chunk.js"
+  },
+  {
+    "revision": "62ec406c8dc10748b489",
+    "url": "./static/js/runtime-main.f927ff14.js"
+  }
+]);
